1ddd7ce96e6fe9e0757526e8decc30baa4c0bea8
DmitryZubarev <justposeur.pr@gmail.com>
2023-01-09 18:20:38 +0500

    Переделал валидацию с помощью pydantic

--- main.py
+++ main.py
@@ -1,46 +1,9 @@
 from fastapi import FastAPI
 from schemas import User
-import re
 from db import get_user, add_user, get_user_by_data
-import json
 
 
 app = FastAPI()
-
-
-def validate(login: str, email: str, phone: str):
-    res = True
-    error = ""
-
-    # login
-    if 3 <= len(login) <= 20:
-        for char in login:
-            if not (97 <= ord(char) <= 122 or 48 <= ord(char) <= 57 or ord(char) == 46):
-                res = False
-                error += "Incorrect login. "
-    else:
-        error += "Incorrect login. "
-        res = False
-
-    # email
-    result = re.split("@", email)
-    if result[0] == '' or result[1] == '':
-        result.remove('')
-    if (not len(result) == 2) or result[0][0] == '.':
-        error += "Incorrect email. "
-        res = False
-
-    # phone
-    if len(phone) == 12 and phone[0:3] == "+79":
-        for char in phone[3:]:
-            if not (48 <= ord(char) <= 57):
-                error += "Incorrect phone. "
-                res = False
-    else:
-        error += "Incorrect phone start or len. "
-        res = False
-
-    return res, error
 
 
 @app.get("/")
@@ -50,13 +13,9 @@ async def root():
 
 @app.post("/check")
 async def check(user: User):
-    val, er = validate(user.login, user.email, user.phone)
-    if val:
-        add_user(user.login, user.email, user.phone)
-        user_id = get_user_by_data(user.login, user.email, user.phone)
-        return {'id': user_id}
-    else:
-        return er
+    add_user(user.login, user.email, user.phone)
+    user_id = get_user_by_data(user.login, user.email, user.phone)
+    return {'id': user_id}
 
 
 @app.get("/users/{user_id}")
--- schemas.py
+++ schemas.py
@@ -1,7 +1,37 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
+import re
 
 
 class User(BaseModel):
     email: str
     login: str
     phone: str
+
+    @validator('email')
+    def check_email(cls, v):
+        result = re.split("@", v)
+        if (not len(result) == 2) or result[0][0] == '.':
+            raise ValueError('Email must be must be in the form user@domain')
+        if result[0] == '' or result[1] == '':
+            raise ValueError('Email must be must be in the form user@domain')
+        return v
+
+    @validator('login')
+    def check_login(cls, v):
+        if 3 <= len(v) <= 20:
+            for char in v:
+                if not (97 <= ord(char) <= 122 or 48 <= ord(char) <= 57 or ord(char) == 46):
+                    raise ValueError('The login must include only the characters a-z 0-9 and .')
+        else:
+            raise ValueError('The login must include only the characters a-z 0-9 and .')
+        return v
+
+    @validator('phone')
+    def check_phone(cls, v):
+        if len(v) == 12 and v[0:3] == "+79":
+            for char in v[3:]:
+                if not (48 <= ord(char) <= 57):
+                    raise ValueError('Phone should be in the form +79XXXXXXXXX')
+        else:
+            raise ValueError('Phone should be in the form +79XXXXXXXXX')
+        return v
